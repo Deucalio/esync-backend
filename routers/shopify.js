7d5563146f5c059090b5eaa8416f54bb08f64090
Deucalio <hjamad89@gmail.com>
2024-02-06 19:01:13 +0500

    add myraid of routes

--- routers/shopify.js
+++ routers/shopify.js
@@ -1,26 +1,11 @@
 const router = require("express").Router();
 const { PrismaClient } = require("../generated/client"); // Adjust the path based on your project structure
-const cloudinary = require('cloudinary');
+const cloudinary = require("cloudinary");
 const prisma = new PrismaClient();
-router.post("/save-token", async (req, res) => {
-  const { email, token } = req.body;
-  try {
-    const updatedUser = await prisma.user.update({
-      where: {
-        email: email,
-      },
-      data: {
-        token: token,
-      },
-    });
-    console.log("updatedUser", updatedUser);
-    return res.status(200).json({ message: "Token saved successfully" });
-  } catch (e) {
-    console.log("Error is: ", e);
-    return res.status(500).json({ message: "Error saving token", e: e });
-  }
-});
 
+// Add a Store in esync/settings
+
+// Validate the name first
 router.post("/validate-name", async (req, res) => {
   // check if the store name is already taken
   const { shopName } = req.body;
@@ -36,17 +21,116 @@ router.post("/validate-name", async (req, res) => {
   res.status(200).json({ message: "Store name available" });
 });
 
-// DELETE A STORE
-router.delete("/delete-store", async (req, res) => {
-  const { email } = req.body;
+// Send the Token, Add the Name and the Image URL,public ID in to the store table
+router.post("/save-token", async (req, res) => {
+  const { email, token, storeName, imageURL, publicID } = req.body;
+  try {
+    // check if the store already exists
+    const storeExists = await prisma.store.findUnique({
+      where: {
+        name: storeName,
+      },
+    });
+    if (storeExists) {
+      // if the store already exists, update the token
+      const store = await prisma.store.update({
+        where: {
+          name: storeName,
+        },
+        data: {
+          token: token,
+        },
+      });
+      return res.status(200).json({ message: "Token updated successfully" });
+    }
 
-  const user = await prisma.user.findUnique({
+    // Find the user by email
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    const store = await prisma.store.create({
+      data: {
+        user_id: user.id, // Specify the userId for the associated user
+        name: storeName,
+        image_url: imageURL,
+        image_public_id: publicID,
+        store_info: { key: "value" },
+        token: token,
+      },
+    });
+    return res.status(200).json({ message: "Store Added successfully" });
+  } catch (e) {
+    console.log("Error is: ", e);
+    return res.status(500).json({ message: "Error saving token", e: e });
+  } finally {
+    await prisma.$disconnect();
+  }
+});
+
+// This is the route Remix will call to and send the store info
+router.post("/save-store-info", async (req, res) => {
+  const { token, store_info } = req.body;
+  // Match the token First
+
+  const store = await prisma.store.findUnique({
     where: {
-      email: email,
+      token: token,
     },
   });
-  console.log("id", user.id);
-  res.send(user);
+
+  if (!store) {
+    return res.status(400).json({ message: "Invalid token" });
+  }
+
+  // Update the store info
+  const updatedStore = await prisma.store.update({
+    where: {
+      token: token,
+    },
+    data: {
+      store_info: store_info,
+    },
+  });
+  res.status(200).json({ message: "Store info updated successfully" });
+});
+
+// ________________
+
+// DELETE A STORE
+router.delete("/delete-store", async (req, res) => {
+  const { storeName } = req.body;
+
+  const storeExists = await prisma.store.findUnique({
+    where: {
+      name: storeName,
+    },
+  });
+  if (!storeExists) {
+    return res.status(400).json({ message: "Store does not exist" });
+  }
+
+  // Get the Public ID of the image
+  const publicID = storeExists.image_public_id;
+
+  // Delete the image from cloudinary
+  cloudinary.uploader.destroy(publicID, (error, result) => {
+    if (error) {
+      console.error("Error deleting image:", error);
+    } else {
+      console.log("Image deleted:", result);
+    }
+  });
+  // Delete the store
+  const store = await prisma.store.delete({
+    where: {
+      name: storeName,
+    },
+  });
+
+  res.status(200).json({ message: "Store deleted successfully" });
 });
 
 router.get("/", async (req, res) => {
