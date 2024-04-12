6a0d0d6b0bf7078e0cde61aec681b78ac9eeff8f
Deucalio <hjamad89@gmail.com>
2024-04-12 18:01:35 +0500

    remove daraz

--- index.js
+++ index.js
@@ -343,7 +343,7 @@ app.post("/orders", async (req, res) => {
   for (const store of userStores) {
     if (store.store_info.platform === "shopify") {
       const response = await axios.get(
-        `https://${store.store_info.shop}/admin/api/2023-10/orders.json?status=open&limit=200`,
+        `https://${store.store_info.shop}/admin/api/2023-10/orders.json?status=open&limit=250`,
         // &financial_status=any
         // &fulfillment_status=unfulfilled
 
