93b364e62d2ad2ed3ddcea725a1a9ce95ed45a27
Deucalio <hjamad89@gmail.com>
2024-05-04 18:53:29 +0500

    change limit, imporve p

--- index.js
+++ index.js
@@ -353,7 +353,7 @@ app.post("/orders", async (req, res) => {
       const darazOrders = await prisma.darazOrders.findMany({
         where: { seller_id: store.store_info.user_info.seller_id },
         orderBy: { created_at: "desc" },
-        take: 500,
+        take: 300,
       });
 
       darazOrders.forEach((order) => {
