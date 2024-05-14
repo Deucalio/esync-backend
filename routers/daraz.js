1c82af68d58ec20cb4671cd0acf19bb454fe2cd8
Deucalio <hjamad89@gmail.com>
2024-05-14 16:20:19 +0500

    add status 404

--- routers/daraz.js
+++ routers/daraz.js
@@ -621,7 +621,7 @@ router.get("/orders/sync", async (req, res) => {
     },
   });
   if (!store || store.length === 0) {
-    return res.status(200).json({ message: "Store not found", seller_id });
+    return res.status(404).json({ message: "Store not found", seller_id });
   }
 
   const { access_token } = store.store_info;
