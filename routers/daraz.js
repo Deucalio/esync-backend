081f72c701e61fbb24767ed32ee361d605069e30
Deucalio <hjamad89@gmail.com>
2024-05-22 17:38:45 +0500

    use for each

--- routers/daraz.js
+++ routers/daraz.js
@@ -795,69 +795,27 @@ router.post("/rts", async (req, res) => {
     });
 
     let result = await Promise.all(requests);
-    // result.forEach((r, i) => {
-    //   const order = rtsData[s].orders[i];
-
-    //   let result_ = "";
-    //   if (r.data.code === "82") {
-    //     // This code occurs when atleast one of the order items is not in the RTS state
-    //     // So we need to sync this order, perhaps this order is canceled
-    //     const url = `https://esync-backend.vercel.app/daraz/orders/sync?seller_id=${s}&order_id=${order.order_id}`;
-    //     result_ = axios.get(url);
-    //     console.log(`Synced cancelled Order: ${order.order_id}`);
-
-    //     RTSed[store].cancelled_orders.data.push(order.order_id);
-    //     RTSed[store].cancelled_orders.count =
-    //       RTSed[store].cancelled_orders.data.length;
-    //   }
-
-    //   if (r.data.code === "0") {
-    //     RTSed[store].rts_orders.data.push(order.order_id);
-    //     RTSed[store].rts_orders.count = RTSed[store].rts_orders.data.length;
-    //   }
-
-    //   // if (!RTSed[store]) {
-    //   //   RTSed[store] = [];
-    //   //   RTSed[store].push(order.order_id);
-    //   // } else {
-    //   //   RTSed[store].push(order.order_id);
-    //   //   RTSed[store]["orders_count"] = RTSed[s].length;
-    //   // }
-
-    //   if (i === result.length - 1 && s === seller_ids[seller_ids.length - 1]) {
-    //     // Add to db
-    //     prisma.temporaryData
-    //       .create({
-    //         data: {
-    //           email: "idk@gmail.com",
-    //           data: RTSed,
-    //         },
-    //       })
-    //       .then((a) => {
-    //         console.log("Added to DB: ", a);
-    //       });
-    //     console.log("RTSed: ", RTSed);
-    //   }
-    // });
-
-    for (let i = 0; i < result.length; i++) {
-      const r = result[i];
+    result.forEach((r, i) => {
       const order = rtsData[s].orders[i];
+
       let result_ = "";
       if (r.data.code === "82") {
         // This code occurs when atleast one of the order items is not in the RTS state
         // So we need to sync this order, perhaps this order is canceled
         const url = `https://esync-backend.vercel.app/daraz/orders/sync?seller_id=${s}&order_id=${order.order_id}`;
-        result_ = await axios.get(url);
+        result_ = axios.get(url);
         console.log(`Synced cancelled Order: ${order.order_id}`);
+
         RTSed[store].cancelled_orders.data.push(order.order_id);
         RTSed[store].cancelled_orders.count =
           RTSed[store].cancelled_orders.data.length;
       }
+
       if (r.data.code === "0") {
         RTSed[store].rts_orders.data.push(order.order_id);
         RTSed[store].rts_orders.count = RTSed[store].rts_orders.data.length;
       }
+
       // if (!RTSed[store]) {
       //   RTSed[store] = [];
       //   RTSed[store].push(order.order_id);
@@ -865,19 +823,22 @@ router.post("/rts", async (req, res) => {
       //   RTSed[store].push(order.order_id);
       //   RTSed[store]["orders_count"] = RTSed[s].length;
       // }
+
       if (i === result.length - 1 && s === seller_ids[seller_ids.length - 1]) {
         // Add to db
-        const add = await prisma.temporaryData.create({
-          data: {
-            email: "idk@gmail.com",
-            data: RTSed,
-          },
-        });
-
+        prisma.temporaryData
+          .create({
+            data: {
+              email: "idk@gmail.com",
+              data: RTSed,
+            },
+          })
+          .then((a) => {
+            console.log("Added to DB: ", a);
+          });
         console.log("RTSed: ", RTSed);
-        console.log("Added to DB: ", add);
       }
-    }
+    });
   }
   const end = new Date().getTime();
   const timeTaken = (end - start) / 1000;
